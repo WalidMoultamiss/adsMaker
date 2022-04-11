5273dcd9f9bfa8146058afe870fcadaa647d5b5a
walid moultamiss <walidmoultamis@gmail.com>
2022-04-11 05:26:01 +0000

    fix @/hooks

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
-import { useOnClickOutside } from "@/hooks";
+import { useOnClickOutside } from "/src/Hooks/index.ts";
 import { AnimatePresence } from "framer-motion";
 import CircularProgress from "@mui/material/CircularProgress";
 
