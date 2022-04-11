4bb9bbc77db3c2bd78a49275548eb2a95c51d6dd
walid moultamiss <walidmoultamis@gmail.com>
2022-04-11 01:37:10 +0000

    fix table

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export {Header} from './header';
 export {DashboardContent} from './DashboardContent';
 export {DashboardOptions} from './DashboardOptions';
-export {TableDBAdmin} from './TableDBAdmin';
 export {TableNew} from './TableNew';
 export {TableDrivers} from './TableDrivers';
 export {Mapbox} from './Mapbox';
